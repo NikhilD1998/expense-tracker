0805e059b3a7155717fd92480bc39e059eaac8c0
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-01-10 22:43:33 +0530

    Fixed blank page issue

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,22 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const expenseMonth = props.date.toLocaleString("en-US", { month: "long" });
+  const expenseYear = props.date.getFullYear();
+  const expenseDay = props.date.toLocaleString('en-US', {day:'2-digit'});
 
   return (
-      <div className='expense-item'>
-          <div>{props.date.toLocaleString()}</div>
-          <div className='expense-item__description'>
-              <h2>{props.title}</h2>
-              <div className='expense-item__price'>${props.amount}</div>
-          </div>
+    <div className="expense-item">
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseYear}</div>
+        <div>{expenseDay}</div>
       </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
   );
 }
 
