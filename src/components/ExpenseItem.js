1a1404e7fb7789e5c9b046fe76c9587603fd312c
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-01-11 21:41:49 +0530

    Changed UI

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const Month = props.date.toLocaleString("en-US", { month: "long" });
-  const Year = props.date.getFullYear();
-  const Day = props.date.toLocaleString('en-US', {day:'2-digit'});
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{Month}</div>
-        <div>{Year}</div>
-        <div>{Day}</div>
-      </div>
+      <ExpenseDate date = {props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
