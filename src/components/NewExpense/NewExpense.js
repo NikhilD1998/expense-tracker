4d5a6446d3d3b7895ac23f970717811742958369
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-02-04 13:48:33 +0530

    Form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import ExpenseForm from './Expenseform';
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return <div>
+        <ExpenseForm></ExpenseForm>
+    </div>
+}
+
+export default NewExpense;
