2e5acecfbdf4a07fcebd108a5b7974611f8235b4
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-02-10 09:18:05 +0530

    Collapsible Form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,38 @@
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        }
-        props.onAddExpense(expenseData)
-    }
-    return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}></ExpenseForm>
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false)
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEdidtingHandler = () => {
+      setIsEditing(false)
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button className="" onClick={startEditingHandler}>
+          Add New Expense
+        </button>
+      )}
+      {isEditing && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel = {stopEdidtingHandler}></ExpenseForm>
+      )}
     </div>
-}
+  );
+};
 
 export default NewExpense;
